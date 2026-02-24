c4b54ed89c43ecadee3d3760946db6bc82371ced
karimsemmoud <karim.semmoud@netoum.com>
2026-02-24 17:06:53 +0700

    start as a lib

--- lib/e2e/application.ex
+++ lib/e2e/application.ex
@@ -7,19 +7,19 @@ defmodule E2e.Application do
 
   @impl true
   def start(_type, _args) do
-    children = [
-      E2eWeb.Telemetry,
-      E2e.Repo,
-      {DNSCluster, query: Application.get_env(:corex_web, :dns_cluster_query) || :ignore},
-      {Phoenix.PubSub, name: E2e.PubSub},
-      # Start a worker by calling: E2e.Worker.start_link(arg)
-      # {E2e.Worker, arg},
-      # Start to serve requests, typically the last entry
-      E2eWeb.Endpoint
-    ]
+    children =
+      if Application.get_env(:corex_web, :start_services, true) do
+        [
+          E2eWeb.Telemetry,
+          E2e.Repo,
+          {DNSCluster, query: Application.get_env(:corex_web, :dns_cluster_query) || :ignore},
+          {Phoenix.PubSub, name: E2e.PubSub},
+          E2eWeb.Endpoint
+        ]
+      else
+        []
+      end
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: E2e.Supervisor]
     Supervisor.start_link(children, opts)
   end
@@ -28,7 +28,10 @@ defmodule E2e.Application do
   # whenever the application is updated.
   @impl true
   def config_change(changed, _new, removed) do
-    E2eWeb.Endpoint.config_change(changed, removed)
+    if Application.get_env(:corex_web, :start_services, true) do
+      E2eWeb.Endpoint.config_change(changed, removed)
+    end
+
     :ok
   end
 end
--- lib/e2e_web/endpoint.ex
+++ lib/e2e_web/endpoint.ex
@@ -37,10 +37,6 @@ defmodule E2eWeb.Endpoint do
     plug Phoenix.Ecto.CheckRepoStatus, otp_app: :corex_web
   end
 
-  plug Phoenix.LiveDashboard.RequestLogger,
-    param_key: "request_logger",
-    cookie_key: "request_logger"
-
   plug Plug.RequestId
   plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
 
